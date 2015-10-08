fc343608ad21f3bdf1776e185d7cf5bc5bccad83
Indrek Ots <indrek.ots@easypark.net>
2015-10-08 14:45:43 +0300

    update data and data import

--- data/import_eventserver.py
+++ data/import_eventserver.py
@@ -21,6 +21,19 @@ def import_events(client, file):
         "attr0" : int(attr[0]),
         "attr1" : int(attr[1]),
         "attr2" : int(attr[2]),
+        "attr3" : int(attr[3]),
+        "attr4" : int(attr[4]),
+        "attr5" : int(attr[5]),
+        "attr6" : int(attr[6]),
+        "attr7" : int(attr[7]),
+        "attr8" : int(attr[8]),
+        "attr9" : int(attr[9]),
+        "attr10" : int(attr[10]),
+        "attr11" : int(attr[11]),
+        "attr12" : int(attr[12]),
+        "attr13" : int(attr[13]),
+        "attr14" : int(attr[14]),
+        "attr15" : int(attr[15]),
         "plan" : int(plan)
       }
     )
